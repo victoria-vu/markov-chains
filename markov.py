f77ba54972013245566d89c4864ee8c57c98168f
Victoria Vu <victoriamvvu@gmail.com>
2022-12-20 22:20:00 -0800

    Completed open_and_read_file and make_chains functions

--- markov.py
+++ markov.py
@@ -1,7 +1,7 @@
 """Generate Markov text from text files."""
 
 from random import choice
-
+# choice()
 
 def open_and_read_file(file_path):
     """Take file path as string; return text as string.
@@ -9,10 +9,12 @@ def open_and_read_file(file_path):
     Takes a string that is a file path, opens the file, and turns
     the file's contents as one string of text.
     """
-
-    # your code goes here
-
-    return 'Contents of your file as one long string'
+    # open the file using "file_path"
+    # return string as text using open("filename").read()
+    
+    file = open(file_path).read().split() #can add .read and .split together -
+    #this read the entire file per item
+    return file
 
 
 def make_chains(text_string):
@@ -21,7 +23,7 @@ def make_chains(text_string):
     A chain will be a key that consists of a tuple of (word1, word2)
     and the value would be a list of the word(s) that follow those two
     words in the input text.
-
+    
     For example:
 
         >>> chains = make_chains('hi there mary hi there juanita')
@@ -38,23 +40,74 @@ def make_chains(text_string):
 
         >>> chains[('there','juanita')]
         [None]
+        #nothing follows juanita
     """
-
+    
+    #can loop over list 2 words at a time by index instead of item with range
+        # for i in range (len(text) - 1): 
+            # print text [i], text[i + 1] 
+        #create tuple of keys (word1, word2) using the for loop
+    #create list of values that follow those two words (word1, word2)
+    #chain = tuple(key) and list(values)
+        # exclude the last bigram as key in chains dictionary
+    # .keys ? sorted(_.keys())?
+    # 
+    #return dict of chains
+    #
     chains = {}
 
-    # your code goes here
-
+    for i in range(len(text_string) - 2): #gets rid of last items
+        green_eggs = (text_string[i], text_string[i + 1]) #made into tuple - set to this variable 
+        if green_eggs in chains: # Appending the value to an existing key 
+            chains[green_eggs].append(text_string[i+2])
+        else: # Creating a new key 
+            chains[green_eggs] = [text_string[i+2]] #tuple became key (green_eggs), value set to word after tuple
+        #dict[key] = [string[value]] means key and value
+        #had an error because - 2 had to match +2 so that it didnt loop farther than the list
+    
+    print(chains)
     return chains
+
+    
 
 
 def make_text(chains):
     """Return text from chains."""
 
-    words = []
+    words = [] # container
 
-    # your code goes here
+    # create a link (link is a key from our dictionary and a random word from the list of words that follow it)
+        # you can use random's choice function (choice()) to pick a random key from chains.keys()
+        # current_key
+    # put the words from that link in a container
+        #chosen_word ?
+    # make a new key out of the second word in the first key and the random word you pulled out from the list of words that followed it
+        #new_key
 
+    key_list = chains.keys() #took all keys out
+    key_list = list(key_list) #made into a list
+
+    #link = key + random word from list of words that follow 
+    for chosen_word in words:
+        words[chosen_word] = choice(key_list), # chosen_word is a random key from the key list
+        words[chosen_random_word] = choice(chains[chosen_word]) # a random word from list of words that follow that key
+        
+    
+    # word = [(Would, you, could)]
+    # random_key = choice(key_list) 
+    # random_key.add(choice(chains[random_key]))
+     
+
+
+    print(words)
     return ' '.join(words)
+
+# myTuple = ("John", "Peter", "Vicky")
+# x = "#".join(myTuple)
+# print(x)
+# John#Peter#Vicky
+
+
 
 
 input_path = 'green-eggs.txt'
